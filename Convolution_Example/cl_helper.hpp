6a9b978c1373a6bf3ae13845e67d363e8108c001
drtpotter <tobympotter@gmail.com>
2021-11-23 21:47:08 +0800

    latest

--- Convolution_Example/cl_helper.hpp
+++ Convolution_Example/cl_helper.hpp
@@ -1,32 +1,87 @@
 #include <iostream>
 
-void errchk(cl_int errcode, std::string message) {
+#ifdef __APPLE__
+    #include "OpenCL/opencl.h"
+#else
+    #include "CL/cl.hpp"
+#endif
+
+#define OCL_EXIT -20
+
+void h_errchk(cl_int errcode, std::string message) {
     if (errcode!=CL_SUCCESS) {
         printf("Error, OpenCL call, failed at %s with error code %d \n", message.c_str(), errcode);
         exit(OCL_EXIT);
     }
 };
 
-void report_on_device(cl_device_id device) {
+cl_command_queue* h_create_command_queues(
+        // Create a list of command queues
+        // with selectable properties
+        // Assumes that contexts is as long as devices
+        cl_device_id *devices,
+        cl_context *contexts, 
+        cl_uint num_devices, 
+        cl_uint num_command_queues,
+        cl_bool out_of_order_enable,
+        cl_bool profiling_enable) {
+    
+    cl_int ret_code;   
+
+    // Manage bit fields
+    cl_command_queue_properties queue_properties = 0;
+    if (out_of_order_enable == CL_TRUE) {
+        queue_properties = queue_properties | CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE;    
+    }
+    if (profiling_enable == CL_TRUE) {
+        queue_properties = queue_properties | CL_QUEUE_PROFILING_ENABLE;    
+    }
+
+    cl_command_queue *command_queues = (cl_command_queue*)malloc(num_command_queues*sizeof(cl_command_queue));
+
+    for (cl_uint n=0; n<num_command_queues; n++) {
+        command_queues[n] = clCreateCommandQueue(
+            contexts[n % num_devices],
+            devices[n % num_devices],
+            queue_properties,
+            &ret_code    
+        );
+        h_errchk(ret_code, "Creating a context");        
+    }
+            
+    return command_queues;
+}
+
+void h_release_command_queues(cl_command_queue *command_queues, cl_uint num_command_queues) {
+    // Release command queues
+    for (cl_uint n = 0; n<num_command_queues; n++) {
+        h_errchk(clFinish(command_queues[n]), "Finishing up command queues");
+        h_errchk(clReleaseCommandQueue(command_queues[n]), "Releasing command queues");
+    }
+
+    // Now destroy the command queues
+    free(command_queues);
+}
+
+void h_report_on_device(cl_device_id device) {
     using namespace std;
 
     // Report some information on the device
     size_t nbytes_name;
-    errchk(clGetDeviceInfo(device, CL_DEVICE_NAME, NULL, NULL, &nbytes_name),"Device name bytes");
+    h_errchk(clGetDeviceInfo(device, CL_DEVICE_NAME, 0, NULL, &nbytes_name),"Device name bytes");
     char* name=new char[nbytes_name];
-    errchk(clGetDeviceInfo(device, CL_DEVICE_NAME, nbytes_name, name, NULL),"Device name");
+    h_errchk(clGetDeviceInfo(device, CL_DEVICE_NAME, nbytes_name, name, NULL),"Device name");
     int textwidth=16;
 
     printf("\t%20s %s \n","name:", name);
 
-
     cl_ulong mem_size;
-    errchk(clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_ulong), &mem_size,
+    h_errchk(clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(cl_ulong), &mem_size,
     NULL),"Global mem size");
 
     printf("\t%20s %d MB\n","global memory size:",mem_size/(1000000));
 
-    errchk(clGetDeviceInfo(device, CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(cl_ulong), &mem_size,
+    h_errchk(clGetDeviceInfo(device, CL_DEVICE_MAX_MEM_ALLOC_SIZE, sizeof(cl_ulong), &mem_size,
     NULL),"Max mem alloc size");
    
     printf("\t%20s %d MB\n","max buffer size:", mem_size/(1000000));
@@ -34,7 +89,7 @@ void report_on_device(cl_device_id device) {
 }
 
 
-void acquire_devices(
+void h_acquire_devices(
         // Input parameter
         cl_device_type device_type,
         // Output parameters
@@ -42,8 +97,7 @@ void acquire_devices(
         cl_uint *num_platforms_out,
         cl_device_id **device_ids_out,
         cl_uint *num_devices_out, 
-        cl_context **contexts_out,
-        cl_uint *num_contexts_out) {
+        cl_context **contexts_out) {
 
     // Return code for running things
     cl_int ret_code = CL_SUCCESS;
@@ -51,17 +105,17 @@ void acquire_devices(
     // Get platforms
     cl_uint num_platforms;
     cl_platform_id *platform_ids = NULL;
-    errchk(clGetPlatformIDs(0, NULL, &num_platforms), "Fetching number of platforms");
-    platform_ids = malloc(num_platforms * sizeof(cl_platform_id));
-    errchk(clGetPlatformIDs(num_platforms, platform_ids, NULL), "Fetching platforms");
+    h_errchk(clGetPlatformIDs(0, NULL, &num_platforms), "Fetching number of platforms");
+    platform_ids = (cl_platform_id*)malloc(num_platforms * sizeof(cl_platform_id));
+    h_errchk(clGetPlatformIDs(num_platforms, platform_ids, NULL), "Fetching platforms");
         
     // Fetch the total number of compatible devices
-    cl_uint ndevices=0;
+    cl_uint num_devices=0;
     
     // Get the total number of devices
     for (cl_uint n=0; n < num_platforms; n++) {
         cl_uint ndevices;
-        errchk(clGetDeviceIDs(
+        h_errchk(clGetDeviceIDs(
             platform_ids[n],
             device_type,
             0,
@@ -71,8 +125,8 @@ void acquire_devices(
     }
     
     // Allocate memory for device ID's and contexts
-    cl_device_id *device_ids = malloc(num_devices*sizeof(cl_device_id));
-    cl_context *contexts = malloc(num_devices*sizeof(cl_context));
+    cl_device_id *device_ids = (cl_device_id*)malloc(num_devices*sizeof(cl_device_id));
+    cl_context *contexts = (cl_context*)malloc(num_devices*sizeof(cl_context));
     
     cl_device_id *device_ids_ptr = device_ids;
     cl_context *contexts_ptr = contexts;
@@ -80,7 +134,7 @@ void acquire_devices(
     // Fill device ID's array
     for (cl_uint n=0; n < num_platforms; n++) {
         cl_uint ndevices;
-        errchk(clGetDeviceIDs(
+        h_errchk(clGetDeviceIDs(
             platform_ids[n],
             device_type,
             0,
@@ -88,7 +142,7 @@ void acquire_devices(
             &ndevices), "Getting number of devices for the platform");
         
         // Fill devices
-        errchk(clGetDeviceIDs(
+        h_errchk(clGetDeviceIDs(
             platform_ids[n],
             device_type,
             ndevices,
@@ -113,7 +167,7 @@ void acquire_devices(
                 NULL,
                 &ret_code
             );
-            errchk(ret_code, "Creating a context");
+            h_errchk(ret_code, "Creating a context");
             contexts_ptr++;
         }
         
@@ -128,27 +182,19 @@ void acquire_devices(
     *device_ids_out = device_ids;
     *num_devices_out = num_devices;
     *contexts_out = contexts;
-    *num_contexts_out = num_devices;
 }
 
-void release_devices(
-        cl_command_queue *command_queues,
-        cl_uint num_command_queues,
+void h_release_devices(
+        cl_device_id *devices,
+        cl_uint num_devices,
         cl_context* contexts,
-        cl_uint num_contexts,
-        cl_platform_id *platforms,
-        cl_device_id *devices) {
+        cl_platform_id *platforms) {
     
-    for (cl_uint n = 0; n<num_command_queues; n++) {
-        errchk(clFinish(command_queues[n]), "Finishing up command queues");
-        errchk(clReleaseCommandQueue(command_queues[n]), "Releasing command queues");
+    // Free contexts
+    for (cl_uint n = 0; n<num_devices; n++) {
+        h_errchk(clReleaseContext(contexts[n]), "Releasing contexts");
     }
 
-    for (cl_uint n = 0; n<num_contexts; n++) {
-        errchk(clReleaseContext(contexts[n]), "Releasing contexts");
-    }
-
-    free(command_queues);
     free(contexts);
     free(devices);
     free(platforms);
