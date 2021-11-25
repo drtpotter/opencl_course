669425b6fb0ab089d9747f9ff1fa17c1311bd651
drtpotter <tobympotter@gmail.com>
2021-11-25 21:36:06 +0800

    latest

--- Convolution_Example/cl_helper.hpp
+++ Convolution_Example/cl_helper.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 
 #ifdef __APPLE__
     #include "OpenCL/opencl.h"
@@ -8,13 +9,81 @@
 
 #define OCL_EXIT -20
 
+
+// Lookup table for error codes
+std::map<cl_int, std::string> error_codes {
+    {CL_SUCCESS, "CL_SUCCESS"},
+    {CL_BUILD_PROGRAM_FAILURE, "CL_BUILD_PROGRAM_FAILURE"},
+    {CL_COMPILE_PROGRAM_FAILURE, "CL_COMPILE_PROGRAM_FAILURE"},
+    {CL_COMPILER_NOT_AVAILABLE, "CL_COMPILER_NOT_AVAILABLE"},
+    {CL_DEVICE_NOT_FOUND, "CL_DEVICE_NOT_FOUND"},
+    {CL_DEVICE_NOT_AVAILABLE, "CL_DEVICE_NOT_AVAILABLE"},
+    {CL_DEVICE_PARTITION_FAILED, "CL_DEVICE_PARTITION_FAILED"},
+    {CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST, "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST"},
+    {CL_IMAGE_FORMAT_MISMATCH, "CL_IMAGE_FORMAT_MISMATCH"},
+    {CL_IMAGE_FORMAT_NOT_SUPPORTED, "CL_IMAGE_FORMAT_NOT_SUPPORTED"},
+    {CL_INVALID_ARG_INDEX, "CL_INVALID_ARG_INDEX"},
+    {CL_INVALID_ARG_SIZE, "CL_INVALID_ARG_SIZE"},
+    {CL_INVALID_ARG_VALUE, "CL_INVALID_ARG_VALUE"},
+    {CL_INVALID_BINARY, "CL_INVALID_BINARY"},
+    {CL_INVALID_BUFFER_SIZE, "CL_INVALID_BUFFER_SIZE"},
+    {CL_INVALID_BUILD_OPTIONS, "CL_INVALID_BUILD_OPTIONS"},
+    {CL_INVALID_COMMAND_QUEUE, "CL_INVALID_COMMAND_QUEUE"},
+    {CL_INVALID_COMPILER_OPTIONS, "CL_INVALID_COMPILER_OPTIONS"},
+    {CL_INVALID_CONTEXT, "CL_INVALID_CONTEXT"},
+    {CL_INVALID_DEVICE, "CL_INVALID_DEVICE"},
+    {CL_INVALID_DEVICE_PARTITION_COUNT, "CL_INVALID_DEVICE_PARTITION_COUNT"},
+    {CL_INVALID_DEVICE_TYPE, "CL_INVALID_DEVICE_TYPE"},
+    {CL_INVALID_EVENT, "CL_INVALID_EVENT"},
+    {CL_INVALID_EVENT_WAIT_LIST, "CL_INVALID_WAIT_LIST"},
+    {CL_INVALID_GLOBAL_OFFSET, "CL_INVALID_GLOBAL_OFFSET"},
+    {CL_INVALID_GLOBAL_WORK_SIZE, "CL_INVALID_GLOBAL_WORK_SIZE"},
+    {CL_INVALID_HOST_PTR, "CL_INVALID_HOST_PTR"},
+    {CL_INVALID_IMAGE_DESCRIPTOR, "CL_INVALID_IMAGE_DESCRIPTOR"},
+    {CL_INVALID_IMAGE_FORMAT_DESCRIPTOR, "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR"},
+    {CL_INVALID_IMAGE_SIZE, "CL_INVALID_IMAGE_SIZE"},
+    {CL_INVALID_KERNEL, "CL_INVALID_KERNEL"},
+    {CL_INVALID_KERNEL_ARGS, "CL_INVALID_KERNEL_ARGS"},
+    {CL_INVALID_KERNEL_DEFINITION, "CL_INVALID_KERNEL_DEFINITION"},
+    {CL_INVALID_KERNEL_NAME, "CL_INVALID_KERNEL_NAME"},
+    {CL_INVALID_LINKER_OPTIONS, "CL_INVALID_LINKER_OPTIONS"},
+    {CL_INVALID_MEM_OBJECT, "CL_INVALID_MEM_OBJECT"},
+    {CL_INVALID_OPERATION, "CL_INVALID_OPERATION"},
+    {CL_INVALID_PLATFORM, "CL_INVALID_PLATFORM"},
+    {CL_INVALID_PROGRAM, "CL_INVALID_PROGRAM"},
+    {CL_INVALID_PROGRAM_EXECUTABLE, "CL_INVALID_PROGRAM_EXECUTABLE"},
+    {CL_INVALID_PROPERTY, "CL_INVALID_PROPERTY"},
+    {CL_INVALID_QUEUE_PROPERTIES, "CL_INVALID_QUEUE_PROPERTIES"},
+    {CL_INVALID_SAMPLER, "CL_INVALID_SAMPLER"},
+    {CL_INVALID_VALUE, "CL_INVALID_VALUE"},
+    {CL_INVALID_WORK_DIMENSION, "CL_INVALID_WORK_DIMENSION"},
+    {CL_INVALID_WORK_GROUP_SIZE, "CL_INVALID_WORK_GROUP_SIZE"},
+    {CL_INVALID_WORK_ITEM_SIZE, "CL_INVALID_WORK_ITEM_SIZE"},
+    {CL_KERNEL_ARG_INFO_NOT_AVAILABLE, "CL_KERNEL_ARG_INFO_NOT_AVAILABLE"},
+    {CL_LINK_PROGRAM_FAILURE, "CL_LINK_PROGRAM_FAILURE"},
+    {CL_LINKER_NOT_AVAILABLE, "CL_LINKER_NOT_AVAILABLE"},
+    {CL_MAP_FAILURE, "CL_MAP_FAILURE"},
+    {CL_MEM_COPY_OVERLAP, "CL_MEM_COPY_OVERLAP"},
+    {CL_MEM_OBJECT_ALLOCATION_FAILURE, "CL_MEM_OBJECT_ALLOCATION_FAILURE"},
+    {CL_MISALIGNED_SUB_BUFFER_OFFSET, "CL_MISALIGNED_SUB_BUFFER_OFFSET"},
+    {CL_OUT_OF_HOST_MEMORY, "CL_OUT_OF_HOST_MEMORY"},
+    {CL_OUT_OF_RESOURCES, "CL_OUT_OF_RESOURCES"},
+    {CL_PROFILING_INFO_NOT_AVAILABLE, "CL_PROFILING_INFO_NOT_AVAILABLE"},
+};
+
+// Function to check error code
 void h_errchk(cl_int errcode, std::string message) {
     if (errcode!=CL_SUCCESS) {
-        printf("Error, OpenCL call, failed at %s with error code %d \n", message.c_str(), errcode);
+        if (error_codes.count(errcode)>0) {
+            printf("Error, Opencl call failed at \"%s\" with error code %s\n", message.c_str(), error_codes[errcode].c_str());
+        } else {
+            printf("Error, OpenCL call failed at \"%s\" with error code %d\n", message.c_str(), errcode);
+        }
         exit(OCL_EXIT);
     }
 };
 
+// Function to create command queues
 cl_command_queue* h_create_command_queues(
         // Create a list of command queues
         // with selectable properties
@@ -37,7 +106,7 @@ cl_command_queue* h_create_command_queues(
         queue_properties = queue_properties | CL_QUEUE_PROFILING_ENABLE;    
     }
 
-    cl_command_queue *command_queues = (cl_command_queue*)malloc(num_command_queues*sizeof(cl_command_queue));
+    cl_command_queue *command_queues = (cl_command_queue*)calloc(num_command_queues, sizeof(cl_command_queue));
 
     for (cl_uint n=0; n<num_command_queues; n++) {
         command_queues[n] = clCreateCommandQueue(
@@ -52,6 +121,7 @@ cl_command_queue* h_create_command_queues(
     return command_queues;
 }
 
+// Function to release command queues
 void h_release_command_queues(cl_command_queue *command_queues, cl_uint num_command_queues) {
     // Release command queues
     for (cl_uint n = 0; n<num_command_queues; n++) {
@@ -63,6 +133,7 @@ void h_release_command_queues(cl_command_queue *command_queues, cl_uint num_comm
     free(command_queues);
 }
 
+// Function to report information on a compute device
 void h_report_on_device(cl_device_id device) {
     using namespace std;
 
@@ -88,7 +159,7 @@ void h_report_on_device(cl_device_id device) {
     delete [] name;
 }
 
-
+// Function to create lists of contexts and devices that map to available hardware
 void h_acquire_devices(
         // Input parameter
         cl_device_type device_type,
@@ -106,7 +177,7 @@ void h_acquire_devices(
     cl_uint num_platforms;
     cl_platform_id *platform_ids = NULL;
     h_errchk(clGetPlatformIDs(0, NULL, &num_platforms), "Fetching number of platforms");
-    platform_ids = (cl_platform_id*)malloc(num_platforms * sizeof(cl_platform_id));
+    platform_ids = (cl_platform_id*)calloc(num_platforms, sizeof(cl_platform_id));
     h_errchk(clGetPlatformIDs(num_platforms, platform_ids, NULL), "Fetching platforms");
         
     // Fetch the total number of compatible devices
@@ -114,19 +185,29 @@ void h_acquire_devices(
     
     // Get the total number of devices
     for (cl_uint n=0; n < num_platforms; n++) {
+
         cl_uint ndevices;
-        h_errchk(clGetDeviceIDs(
+        ret_code = clGetDeviceIDs(
             platform_ids[n],
             device_type,
             0,
             NULL,
-            &ndevices), "Getting number of devices");
-        num_devices += ndevices;
+            &ndevices);
+
+        if (ret_code != CL_DEVICE_NOT_FOUND) {
+            h_errchk(ret_code, "Getting number of devices");
+            num_devices += ndevices;
+        }
     }
     
+    if (num_devices == 0) {
+        printf("Failed to find a suitable compute device\n");
+        exit(OCL_EXIT);
+    }
+
     // Allocate memory for device ID's and contexts
-    cl_device_id *device_ids = (cl_device_id*)malloc(num_devices*sizeof(cl_device_id));
-    cl_context *contexts = (cl_context*)malloc(num_devices*sizeof(cl_context));
+    cl_device_id *device_ids = (cl_device_id*)calloc(num_devices, sizeof(cl_device_id));
+    cl_context *contexts = (cl_context*)calloc(num_devices, sizeof(cl_context));
     
     cl_device_id *device_ids_ptr = device_ids;
     cl_context *contexts_ptr = contexts;
@@ -134,45 +215,51 @@ void h_acquire_devices(
     // Fill device ID's array
     for (cl_uint n=0; n < num_platforms; n++) {
         cl_uint ndevices;
-        h_errchk(clGetDeviceIDs(
+
+        ret_code = clGetDeviceIDs(
             platform_ids[n],
             device_type,
             0,
             NULL,
-            &ndevices), "Getting number of devices for the platform");
-        
-        // Fill devices
-        h_errchk(clGetDeviceIDs(
-            platform_ids[n],
-            device_type,
-            ndevices,
-            device_ids_ptr,
-            NULL), "Filling devices");
-        
-        // Create a context for every device found
-        for (cl_uint c=0; c<ndevices; c++ ) {
-            // Context properties
-            const cl_context_properties prop[] = {CL_CONTEXT_PLATFORM, 
-                                                  (cl_context_properties)platform_ids[n], 
-                                                  0 };
-            // Create a context with 1 device in it
-            const cl_device_id dev_id = *(device_ids_ptr+c);
-            cl_uint ndev = 1;
+            &ndevices);
+
+        if (ret_code != CL_DEVICE_NOT_FOUND) {
+            h_errchk(ret_code, "Getting number of devices for the platform");
             
-            *contexts_ptr = clCreateContext(
-                prop, 
-                ndev, 
-                &dev_id,
-                NULL,
-                NULL,
-                &ret_code
-            );
-            h_errchk(ret_code, "Creating a context");
-            contexts_ptr++;
+            // Fill devices
+            h_errchk(clGetDeviceIDs(
+                platform_ids[n],
+                device_type,
+                ndevices,
+                device_ids_ptr,
+                NULL), "Filling devices");
+            
+            // Create a context for every device found
+            for (cl_uint c=0; c<ndevices; c++ ) {
+                // Context properties
+                const cl_context_properties prop[] = { CL_CONTEXT_PLATFORM, 
+                                                      (cl_context_properties)platform_ids[n], 
+                                                      0 };
+                
+                // Create a context with 1 device in it
+                const cl_device_id dev_id = *(device_ids_ptr+c);
+                cl_uint ndev = 1;
+                
+                *contexts_ptr = clCreateContext(
+                    prop, 
+                    ndev, 
+                    &dev_id,
+                    NULL,
+                    NULL,
+                    &ret_code
+                );
+                h_errchk(ret_code, "Creating a context");
+                contexts_ptr++;
+            }
+            
+            // Advance device_id's pointer
+            device_ids_ptr += ndevices;
         }
-        
-        // Advance device_id's pointer
-        device_ids_ptr += ndevices;
     }   
 
     // Fill in output information here to 
@@ -184,6 +271,7 @@ void h_acquire_devices(
     *contexts_out = contexts;
 }
 
+// Function to release devices and contexts
 void h_release_devices(
         cl_device_id *devices,
         cl_uint num_devices,
